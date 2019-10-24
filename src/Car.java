782016981d3deaef2d9c162d6745ae6437a8e8f1
Leonardo Fontanelli <l.fontanelli2@studenti.unipi.it>
2019-10-24 17:25:39 +0200

    Aggiornamento classe JPA

--- src/Car.java
+++ src/Car.java
@@ -34,7 +34,7 @@ public class Car {
     }
     
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="IdCar", updatable=false, nullable=false )
     public int getIdCar() { return idCar.get();}
     @Column(name="Vendor")
