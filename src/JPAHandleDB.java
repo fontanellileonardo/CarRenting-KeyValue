9714885dcd727ec345eca5501407fa88a5246134
Leonardo Fontanelli <l.fontanelli2@studenti.unipi.it>
2019-10-26 19:58:58 +0200

    Metodi findAvailableCars, insertFeedback, insertReservation (da testare)

--- src/JPAHandleDB.java
+++ src/JPAHandleDB.java
@@ -1,3 +1,5 @@
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.*;
@@ -8,6 +10,11 @@ public class JPAHandleDB {
 	private static EntityManager entityManager;
 	private static String selectAllCustomers = "SELECT u FROM User u WHERE u.customer = true";
 	private static String selectAllFeedbacks = "SELECT f FROM Feedback f WHERE f.mark <= :minMark";
+	private static String selectActiveReservation = "SELECT r FROM Reservation r WHERE r.User = :user AND r.pickUpDate > :pickUpDate";
+	private static String findAvailableCars = "SELECT c FROM Car c WHERE c.location = :location AND c.seatNumber = :seatNumber AND c.idCar NOT IN "
+												+ "(SELECT r.Car FROM Reservation r WHERE (r.pickUpDate BETWEEN :pickUpDate AND :deliveryDate) "
+												+ "OR (r.deliveryDate BETWEEN :pickUpDate AND :deliveryDate) "
+												+ "OR (pickUpDate < :pickUpDate AND deliveryDate > :deliveryDate))";
 	
 	static {
 		factory = Persistence.createEntityManagerFactory("CarRenting");
@@ -129,6 +136,69 @@ public class JPAHandleDB {
 		} finally {
 			entityManager.close();
 		}
+		return result;
+	}
+	
+	public static List<Car> findAvailableCars(LocalDate arrival, LocalDate departure, String loc, String seats){
+		List<Car> result = null;
+		Date arr = java.sql.Date.valueOf(arrival);
+		Date dep = java.sql.Date.valueOf(departure);
+		try {
+			entityManager = factory.createEntityManager();
+			TypedQuery<Car> query = entityManager.createQuery(findAvailableCars, Car.class);
+			query.setParameter("location", loc);
+			query.setParameter("seatNumber", seats);
+			query.setParameter("pickUpDate", arr);
+			query.setParameter("deliveryDate", dep);
+			query.setParameter("pickUpDate", arr);
+			query.setParameter("deliveryDate", dep);
+			query.setParameter("pickUpDate", arr);
+			query.setParameter("deliveryDate", dep);
+			result = query.getResultList();
+		} catch (Exception ex) {
+			System.err.println("Exception while searching for available cars");
+			return null;
+		}
+		finally {
+			entityManager.close();
+		}
+		return result;
+	}
+	
+	public static int selectActiveReservation(Reservation r) {
+		List<Reservation> result = null;
+		try {
+			entityManager = factory.createEntityManager();
+			TypedQuery<Reservation> query = entityManager.createQuery(selectActiveReservation, Reservation.class);
+			query.setParameter("user", r.getUser()); 
+			query.setParameter("pickUpDate", r.getPickUpDate());
+			result = query.getResultList();
+		}
+		catch (Exception ex){
+			System.err.println("Exception during reservation selection: " + ex.getMessage());
+			return 2;
+		}
+		finally {
+			entityManager.close();
+		}
+		if(result == null)
+			return 0;
+		else
+			return 1;
+	}
+	
+	public static int insertNewReservation(Reservation r) { //Ho supposto che si passasse un oggetto Reservation a questo metodo. Se si vuole far passare gli stessi parametri della scorsa versione, si può anche cambiare (o fare un overloading)
+		int reservation = selectActiveReservation(r);
+		if (reservation == 1) {
+			System.err.println("It's not permitted to book more than one car at a time");
+			return 1;
+		}
+		int result = create(r);
+		return result;
+	}
+	
+	public static boolean insertNewFeedback(Feedback f) {
+		int result = create(f);
 		return result;
 	}
 	
