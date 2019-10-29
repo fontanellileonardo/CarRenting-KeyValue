e9894ef0f64315f9266cef78dd25a2010915524f
Carmelo Aparo <carmelo.aparo.ca@gmail.com>
2019-10-29 16:53:18 +0100

    Aggiunto campo removed.

--- src/Car.java
+++ src/Car.java
@@ -6,47 +6,48 @@ import javax.persistence.*;
 @Table(name="Car")
 public class Car {
 	
-    private long idCar; 
+	private final SimpleStringProperty licencePlate;
     private final SimpleStringProperty vendor;
     private final SimpleIntegerProperty seatNumber;
     private final SimpleStringProperty location;
-    private final SimpleIntegerProperty kilometers;
+    private final SimpleDoubleProperty kilometers;
     private final SimpleDoubleProperty price;
-    private final SimpleStringProperty licencePlate;
+    private final SimpleBooleanProperty removed;
     
     private List<Reservation> reservations =	new ArrayList<>();
     
     public Car() {
-        vendor = new SimpleStringProperty("");
+    	licencePlate = new SimpleStringProperty("");
+    	vendor = new SimpleStringProperty("");
         seatNumber = new SimpleIntegerProperty(0);
         location = new SimpleStringProperty("");
-        kilometers = new SimpleIntegerProperty(0);
+        kilometers = new SimpleDoubleProperty(0);
         price = new SimpleDoubleProperty(0.0);
-        licencePlate = new SimpleStringProperty("");
+        removed = new SimpleBooleanProperty(false);
     }
     
-    public Car(String v, int s, String l, int k, Double pr, String p) {
-        vendor = new SimpleStringProperty(v);
+    public Car(String v, int s, String l, int k, Double pr, String p, Boolean r) {
+    	licencePlate = new SimpleStringProperty(p);
+    	vendor = new SimpleStringProperty(v);
         seatNumber = new SimpleIntegerProperty(s);
         location = new SimpleStringProperty(l);
-        kilometers = new SimpleIntegerProperty(k);
+        kilometers = new SimpleDoubleProperty(k);
         price = new SimpleDoubleProperty(pr);
-        licencePlate = new SimpleStringProperty(p);
+        removed = new SimpleBooleanProperty(r);
     }
     
-    public void setIdCar(long i) {idCar = i;}
     public void setVendor(String v) {vendor.set(v);}
     public void setSeatNumber(int s) {seatNumber.set(s);}
     public void setLocation(String l) {location.set(l);}
-    public void setKilometers(int k) {kilometers.set(k);}
+    public void setKilometers(double k) {kilometers.set(k);}
     public void setPrice(Double pr) {price.set(pr);}
     public void setLicencePlate(String p) {licencePlate.set(p);}
+    public void setRemoved(Boolean removed) {this.removed.set(removed);}
     public void setReservations(List<Reservation> res) {reservations = res;}
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="IdCar", updatable=false, nullable=false )
-    public long getIdCar() { return idCar;}
+    @Column(name="LicencePlate", unique = true)
+    public String getLicencePlate() {return licencePlate.get();}
     @Column(name="Vendor")
     public String getVendor() { return vendor.get();}
     @Column(name="SeatNumber")
@@ -54,11 +55,11 @@ public class Car {
     @Column(name="Location")
     public String getLocation() { return location.get();}
     @Column(name="Kilometers")
-    public int getKilometers() {return kilometers.get();}
+    public double getKilometers() {return kilometers.get();}
     @Column(name="Price")
     public Double getPrice() {return price.get();}
-    @Column(name="LicencePlate", unique = true)
-    public String getLicencePlate() {return licencePlate.get();}
+    @Column(name="Removed")
+    public Boolean getRemoved() { return removed.get(); }
     @OneToMany(
     		mappedBy = "car",
     		fetch = FetchType.LAZY,
