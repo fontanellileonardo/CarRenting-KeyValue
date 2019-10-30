63b34065b124da823cd73de5c5f67dcf4c7d2010
Dani <Dani@SuperMario>
2019-10-30 17:43:54 +0100

    Sistemate tabelle a fianco del form per inserire nuova macchina

--- src/EmployerInterface.java
+++ src/EmployerInterface.java
@@ -21,11 +21,12 @@ public class EmployerInterface {
     private final Label title;
     private final Text errorMsg;
     private final Label insertTitle;
-    final Label idCar,vendor,seatNumber,location,kilometers,price;
+    final Label licensePlate,vendor,seatNumber,location,kilometers,price;
     private final Label feedbackTitle;
     private final Label userTitle;
+    private final Label carTitle;
 //	------ TEXT FIELDS ------
-    private final TextField fieldIdCar; 
+    private final TextField fieldLicensePlate; 
     private final TextField fieldVendor; 
     private final TextField fieldKm; 
     private final TextField fieldPrice;
@@ -36,6 +37,7 @@ public class EmployerInterface {
 //	------ TABLES ------
     private final VisualTableFeedback tableFeedback;
     private final VisualTableUser tableUser;
+    private final VisualTableCar tableCar;
 //	------ BUTTONS ------
     final Button insertButton;
     final Button deleteButton;
@@ -43,6 +45,7 @@ public class EmployerInterface {
 //	------ BOXES ------
     private final VBox insertPanel;
     private final VBox userPanel;
+    private final VBox carPanel;
     private final VBox feedbackPanel;
     private final AnchorPane box;
     
@@ -53,7 +56,7 @@ public class EmployerInterface {
         title = new Label("Employer Management");
         errorMsg = new Text();
         insertTitle = new Label("Insert a new car");
-        idCar = new Label("Id Car:");
+        licensePlate = new Label("License Plate:");
         vendor = new Label("Vendor:");
         location = new Label("Location:");
         kilometers = new Label("Kilometers:");
@@ -61,8 +64,9 @@ public class EmployerInterface {
         seatNumber = new Label("Seat Number:");
         feedbackTitle = new Label("FEEDBACKS");
         userTitle = new Label("USERS");
+        carTitle = new Label("CARS");
 //    	------ TEXT FIELDS ------
-        fieldIdCar = new TextField();
+        fieldLicensePlate = new TextField();
         fieldVendor = new TextField();
         fieldKm = new TextField();
         fieldPrice = new TextField();
@@ -85,13 +89,14 @@ public class EmployerInterface {
 //    	------ TABLES ------
         tableFeedback = new VisualTableFeedback();
         tableUser = new VisualTableUser(); //tabella per visualizzare gli users
+        tableCar = new VisualTableCar(false);
 //    	------ BUTTONS ------
         insertButton = new Button("INSERT");
         deleteButton = new Button("DELETE");
         logOutButton = new Button("LOG OUT");
 //    	------ BOXES ------
         insertPanel = new VBox(3);
-        insertPanel.getChildren().addAll(insertTitle, errorMsg,idCar,fieldIdCar,vendor,
+        insertPanel.getChildren().addAll(insertTitle, errorMsg,licensePlate,fieldLicensePlate,vendor,
                 fieldVendor,seatNumber,fieldSeats, location,
                 fieldLocation,kilometers, fieldKm, price, 
                 fieldPrice, insertButton, deleteButton);
@@ -99,14 +104,16 @@ public class EmployerInterface {
         feedbackPanel.getChildren().addAll(feedbackTitle, tableFeedback);
         userPanel = new VBox(DX_PANEL_SPACE); 
         userPanel.getChildren().addAll(userTitle, tableUser);
+        carPanel = new VBox(DX_PANEL_SPACE);
+        carPanel.getChildren().addAll(carTitle, tableCar, deleteButton);
         box = new AnchorPane();
-        box.getChildren().addAll(logOutButton,title, tableChoose, insertPanel);
+        box.getChildren().addAll(logOutButton,title, insertPanel, tableChoose, carPanel);
     }
     
     public void setEmpInterfaceStyle() { 
 //    	------ LABELS ------
         title.setFont(Font.font("Calibri", 11 + TITLE_SIZE)); 
-        title.setLayoutX(50);
+        title.setLayoutX(340);
         title.setLayoutY(10);
         insertTitle.setFont(Font.font("Calibri", 16));
         errorMsg.setFont(Font.font("Calibri", 16));
@@ -114,30 +121,42 @@ public class EmployerInterface {
 //    	------ TABLES ------
         tableFeedback.setTableFeedbackStyle();
         tableUser.setTableUserStyle(); //tabella User
-        //tabella CAR editabile qua
+        tableCar.setTableCarStyle();
 //    	------ BOXES ------
-        insertPanel.setLayoutX(100);
-        insertPanel.setLayoutY(50);
+        insertPanel.setLayoutX(60);
+        insertPanel.setLayoutY(100);
         insertPanel.setPrefSize(300,450);
         insertPanel.setAlignment(Pos.CENTER);
         
-        feedbackPanel.setLayoutX(100);
-        feedbackPanel.setLayoutY(50);
-        feedbackPanel.setPrefSize(300,450);
+        feedbackPanel.setLayoutX(400);
+        feedbackPanel.setLayoutY(100);
+        feedbackPanel.setPrefSize(500,450);
         feedbackPanel.setAlignment(Pos.CENTER);
+        feedbackPanel.fillWidthProperty();
         
-        userPanel.setLayoutX(100);
-        userPanel.setLayoutY(50);
-        userPanel.setPrefSize(300,450);
+        userPanel.setLayoutX(400);
+        userPanel.setLayoutY(100);
+        userPanel.setPrefSize(500,450);
         userPanel.setAlignment(Pos.CENTER);
+        userPanel.fillWidthProperty();
+        
+        carPanel.setLayoutX(400);
+        carPanel.setLayoutY(100);
+        carPanel.setPrefSize(500,450);
+        carPanel.setAlignment(Pos.CENTER);
+        carPanel.fillWidthProperty();
+        
         box.setStyle("-fx-background-color: khaki"); 
         box.setPadding(new Insets(0,5,0,0));
-        box.setLeftAnchor(title, 90.0);
+        box.setPrefWidth(890);
+        box.setPrefHeight(660);
+        box.setTopAnchor(insertPanel,129.0);
+        /*box.setLeftAnchor(title, 90.0);
         box.setTopAnchor(title, 30.0);
         box.setLeftAnchor(logOutButton,430.0);
         box.setTopAnchor(logOutButton,5.0);
         box.setBottomAnchor(deleteButton, 70.0);
-        box.setBottomAnchor(insertButton, 50.0);
+        box.setBottomAnchor(insertButton, 50.0);*/
     }
     
     // listen the events 
@@ -156,7 +175,7 @@ public class EmployerInterface {
         tableChoose.setOnAction((ActionEvent ev)-> { 
         	if(tableChoose.getValue() == "User Table") {
                 if(table == 1) {	
-                	box.getChildren().removeAll(insertPanel);
+                	box.getChildren().removeAll(carPanel);
                 }
                 else if(table == 3) { 
                 	box.getChildren().removeAll(feedbackPanel);
@@ -166,7 +185,6 @@ public class EmployerInterface {
         	}
         	
         	else if(tableChoose.getValue() == "Car Manager") {
-        		//tabella CAR editabile
         		if(table == 2) {
         			box.getChildren().removeAll(userPanel);
         		}
@@ -174,12 +192,12 @@ public class EmployerInterface {
         			box.getChildren().removeAll(feedbackPanel);
         		}
         		table = 1;
-        		box.getChildren().addAll(insertPanel);
+        		box.getChildren().addAll(carPanel);
         	}
         	
         	else if(tableChoose.getValue() == "Feedback Table") {
         		if(table == 1) {
-        			box.getChildren().removeAll(insertPanel);
+        			box.getChildren().removeAll(carPanel);
         		}
         		else if(table == 2) {
         			box.getChildren().removeAll(userPanel);
@@ -197,11 +215,11 @@ public class EmployerInterface {
             int seats = Integer.parseInt(fieldSeats.getValue().toString());
             // Check if all the fields are correct
             String outcome = "";
-            if(fieldIdCar.getText().equals("") == false && fieldVendor.getText().equals("") == false &&
+            if(fieldLicensePlate.getText().equals("") == false && fieldVendor.getText().equals("") == false &&
             		fieldKm.getText().matches("^\\d+$") == true && 
             		fieldPrice.getText().matches("^\\d+\\.?[0-9]*$") == true ) {
             	Car car = new Car(fieldVendor.getText(),seats,loc,Integer.parseInt(fieldKm.getText()),
-            			Double.parseDouble(fieldPrice.getText()),fieldIdCar.getText(), false);
+            			Double.parseDouble(fieldPrice.getText()),fieldLicensePlate.getText(), false);
             	// try to insert new car in the DB
             	outcome = rh.insertCar(car);
                 if(outcome.equals("Success!")) {
@@ -222,7 +240,7 @@ public class EmployerInterface {
     // reset the fields in the Employer Interface
     public void clearAll() {
         
-        fieldIdCar.clear();
+        fieldLicensePlate.clear();
         fieldVendor.clear();
         fieldLocation.setValue("Firenze");
         fieldSeats.setValue("4");
